51786da15f30aecdb41b03874dc5d8e280fb9fe6
ryantetro <ryantetro@RYANTETROFB53>
2024-01-17 12:58:08 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class DiceRoller
+    {
+        private Random random = new Random();
+
+        public int[] RollDice(int numberOfRolls)
+        {
+            int[] counts = new int[11];
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int roll1 = random.Next(1, 7);
+                int roll2 = random.Next(1, 7);
+                int sum = roll1 + roll2;
+
+                counts[sum - 2]++;
+            }
+
+            return counts;
+        }
+
+        public int[] calculatePercentages(int[] counts ,int totalRolls) 
+        { 
+            int[] percentages = new int[counts.Length];
+            
+
+            for (int i = 0;  i < counts.Length; i++) 
+            {
+                percentages[i] = (int)(((double)counts[i] / totalRolls) * 100);
+            }
+
+            return percentages;
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Dice Throwing Simulator! ");
+
+
+        int numberOfRolls;
+
+        do
+        {
+            Console.Write("How many Dice rolls would you like to simulate? ");
+        }
+        while (!int.TryParse(Console.ReadLine(), out numberOfRolls) || numberOfRolls <= 0);
+
+        DiceRoller diceRoller = new DiceRoller();
+
+        int[] counts = diceRoller.RollDice(numberOfRolls);
+        int[] percentages = diceRoller.calculatePercentages(counts, numberOfRolls);
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \" * \" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total Number of rolls = {numberOfRolls}.");
+
+        for (int i = 0; i < percentages.Length; i++)
+        {
+            Console.Write($"{i + 2}: ");
+            Console.WriteLine(new string('*', percentages[i]));
+        }
+
+        Console.WriteLine("Thank you for using the Dice Throwing Simulator. Goodbye! ");
+    }
+}
